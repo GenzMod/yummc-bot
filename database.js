09678dd4440bec7a6d1f0ca6117460dc438daa3e
SmiLerich <sktkiemrs@gmail.com>
2026-03-07 13:40:48 +0700

    jsdfnjdsnf

--- database.js
+++ database.js
@@ -1,19 +1,44 @@
-const fs = require("fs")
+const fs = require("fs");
 
-const file = "./userdata.json"
+const FILE = "./userdata.json";
 
-function loadData(){
-if(!fs.existsSync(file)){
-fs.writeFileSync(file,"{}")
+function load() {
+  if (!fs.existsSync(FILE)) {
+    fs.writeFileSync(FILE, JSON.stringify({ marriages: {} }, null, 2));
+  }
+  return JSON.parse(fs.readFileSync(FILE));
 }
-return JSON.parse(fs.readFileSync(file))
-}
 
-function saveData(data){
-fs.writeFileSync(file, JSON.stringify(data,null,2))
+function save(data) {
+  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
 }
 
 module.exports = {
-loadData,
-saveData
-}
+
+  getMarriage(userId) {
+    const data = load();
+    return data.marriages[userId];
+  },
+
+  marry(user1, user2) {
+    const data = load();
+
+    data.marriages[user1] = user2;
+    data.marriages[user2] = user1;
+
+    save(data);
+  },
+
+  divorce(userId) {
+    const data = load();
+    const partner = data.marriages[userId];
+
+    if (partner) {
+      delete data.marriages[userId];
+      delete data.marriages[partner];
+    }
+
+    save(data);
+  }
+
+};
